b167cf580e1ea036971840e5bbd63be15137fc50
Nasir Hayat <157480294+nasirhayat028@users.noreply.github.com>
2025-03-11 19:33:51 +0000

    Understanding the varaibles with there concepts

---
+++ varaibles.js
@@ -0,0 +1,11 @@
+
+const nameFirst = "Nasir"
+let nameMiddle = "Hayat"
+var nameSecond = "Bangash"
+fullName = "Nasir Hayat"
+// Varaible dec...
+
+/* Prefer not to use var beacuse tomorrow we will face many issues in block scope & function scopes */
+
+console.log(nameFirst); 
+console.table([nameFirst, nameMiddle, nameSecond, fullName])

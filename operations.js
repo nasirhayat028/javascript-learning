747be6af1daba77441105ffe8db4ca0ae59e039c
Nasir Hayat <157480294+nasirhayat028@users.noreply.github.com>
2025-03-12 09:54:58 +0000

    SOme Basic Operation in Java Script that we must know about thesee all as a begginer

---
+++ operations.js
@@ -0,0 +1,36 @@
+
+let value = 20
+
+let negValue = -value;
+
+// negValue
+console.log(negValue);
+
+
+let firstName = "Nasir"
+let secondName = " Hayat"
+
+// addtion of two strings   
+let fullName = firstName + secondName
+
+console.log(fullName);
+
+// Some more basic Operations 
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+console.log(+true);
+console.log(+"");
+
+
+// adding same value to many var...
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+console.log(num2);
+console.log(num3);

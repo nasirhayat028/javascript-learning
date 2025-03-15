b8a1acd0a7368a6121fd0a1cc2293c9acaad5227
Nasir Hayat <157480294+nasirhayat028@users.noreply.github.com>
2025-03-16 00:57:26 +0500

    Understanding all about numbers and Maths basic operation that we can use much more in future or projects x,y,z...

---
+++ mathandnumbers.js
@@ -0,0 +1,31 @@
+
+let num = 1000
+
+console.log(typeof num);
+
+let number = new Number (100)
+
+console.log(number);
+console.log(typeof number);
+
+console.log(number.toString().length);
+
+console.log(number.toFixed(2));
+
+let newNumber = 250.677
+console.log(newNumber.toPrecision(4));
+
+// ++++++++++ MATHS ++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math.ceil(298.23));
+console.log(Math.min(2,4,5,1,0,10));
+console.log(Math.max(2,4,5,1,0,10));
+console.log(Math.min(2,4,5,1,0,10));
+console.log(Math.floor(Math.random()*10 +1));
+
+min = 100
+max =200
+
+console.log(Math.floor(Math.random() *  (max - min +1) +min));

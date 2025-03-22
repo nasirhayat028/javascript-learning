cf93ac24611f4a0e7e519069848094c46dffb0ad
Nasir Hayat <157480294+nasirhayat028@users.noreply.github.com>
2025-03-23 01:32:28 +0500

    Part two on  loops.. While and Do while basic

---
+++ doWhileloop.js
@@ -0,0 +1,28 @@
+
+i = 1
+while (i <= 10) {
+    console.log(`the value is ${i}`);
+    i++
+    
+}
+
+
+// let me find out values from array 
+
+names = ['Nasir', 'Sudais', 'Marwan', 'Roaman']
+
+display = 0
+while (display < names.length) {
+    console.log(names[display]);
+    display ++
+        
+}
+
+// time for .........Do while Loop
+
+value = 1
+do {
+    console.log(`the value is ${value}`);
+    value++
+    
+} while (value < 10);

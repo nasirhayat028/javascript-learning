9d1630ef38b3d41181ec3f16d83fb296ae657ecb
Nasir Hayat <157480294+nasirhayat028@users.noreply.github.com>
2025-03-23 03:06:19 +0500

    Filter and Each understanding

---
+++ filtermap.js
@@ -0,0 +1,16 @@
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let numDetails = number.filter( (num) => num > 4) // Store all those values which is greater than 4
+
+console.log(numDetails);
+
+
+let names = ['Nasir', 'Sudais', 'Marwan', 'Roman']
+
+let namesDetails = names.filter((n)=> n)
+console.log(namesDetails);
+
+let namesDetails2 = names.forEach((n)=> console.log(n) )
+console.log(namesDetails2);
+
